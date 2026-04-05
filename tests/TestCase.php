24ca5066cc4cebc07d15892807b3a5c052de80ec
Gilang Pratama P <pratamapriadi96@gmail.com>
2026-04-05 19:13:38 +0700

    Fix CI test environment setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
+
     public function createApplication(): Application
     {
         /** @var Application $app */
